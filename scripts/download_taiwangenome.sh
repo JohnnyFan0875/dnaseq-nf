6e057e17a7ca23b207373c9f7ce8f10741e82af9
johnny_fan <johnny_fan@mail.asiamedcom.com>
2025-08-29 15:56:15 +0800

    add vep annotation and taiwangenome vcf download process

---
+++ scripts/download_taiwangenome.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p reference/taiwangenome
+
+wget https://zenodo.org/records/16892942/files/taiwangenome.hg38.merged.sort.vcf.gz?download=1 -O reference/taiwangenome/taiwangenome.hg38.merged.sort.vcf.gz
